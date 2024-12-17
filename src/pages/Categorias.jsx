4f512cdd4b1fc2059f7bc51f7c0a2f982fac1bbc
Lucas Romero <lukisromero@hotmail.com.ar>
2024-12-17 13:31:46 -0300

    .

--- src/pages/Categorias.jsx
+++ src/pages/Categorias.jsx
@@ -39,26 +39,24 @@ function Categorias() {
 
         <h1 className="text-2xl text-white font-semibold">{categoria}</h1>
       </header>
-      <div className=" flex  flex-1 items-center  px-4 mx-4  mb-5">
+      <div className=" flex  flex-1 items-center justify-between  px-4 mx-4  mb-5">
         <h3 className="text-lg font-semibold text-left max-w-32 min-w-32 justify-center pl-2">
           Concepto
         </h3>
         <h3 className="text-lg font-medium text-center max-w-20 min-w-20 pl-2">Monto</h3>
-        <p className="max-w-24 min-w-24 text-center">Fecha</p>
+        <p className="text-center max-w-20 min-w-20">Fecha</p>
       </div>
       {gastos.map((gasto) => {
         return (
           <div
             key={gasto._id}
-            className="shadow flex  flex-1 items-center rounded-xl px-4 mx-4 min-h-20 max-h-20 mb-5 overflow-hidden"
+            className="shadow flex  flex-1 items-center justify-between rounded-xl px-4 mx-4 min-h-20 max-h-20 mb-5 overflow-hidden"
           >
             <h3 className="text-lg font-semibold text-left max-w-32 min-w-32 whitespace-wrap overflow-clip">
               {gasto.concepto ? gasto.concepto : `Gasto en ${categoria}`}
             </h3>
             <h3 className="text-lg font-medium text-center max-w-20 min-w-20">${gasto.monto}</h3>
-            <p className="max-w-24 min-w-24 text-center">
-              {new Date(gasto.fecha).toISOString().split("T")[0]}
-            </p>
+            <p className=" text-center">{new Date(gasto.fecha).toISOString().split("T")[0]}</p>
           </div>
         );
       })}
